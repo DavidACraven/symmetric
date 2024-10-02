e32e8daf5786352ce34dd82917e341bef123e6a1
DavidACraven <davcrav@gmail.com>
2024-10-02 15:29:20 +0100

    Add files via upload

---
+++ symmetric.s
@@ -0,0 +1,31 @@
+{
+  Partitions
+  {
+    selfconjugatepartitions.m
+    nextpartition.m
+    hooklengths.m
+    specialpartitions.m
+    displaypartitions.m
+  }
+  Degrees
+  {
+    largestdegrees.m
+    maximummultiplicities.m
+    gldegrees.m
+  }
+  InfinityPartitions
+  {
+    infinitypartitionsdatabase.m
+    periodicclusterdatabase.m
+    infinitypartitions.m
+    clusters.m
+    partitionclusterdatabase.m
+    largepartitionclusterdatabase.m
+  }
+  SylowRestriction
+  {
+    sylowrestriction.m
+    matrixdatabase.m
+    sym128matrix.m
+  }
+}
